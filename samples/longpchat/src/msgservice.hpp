9fb30564f46eb1426e41b8aade5c98fe8f4d68af
Tadeusz <pantadeusz@pjwstk.edu.pl>
2018-01-27 00:26:20 +0100

    allow for sending to specified recipient in sample poll

--- samples/longpchat/src/msgservice.hpp
+++ samples/longpchat/src/msgservice.hpp
@@ -24,26 +24,24 @@ protected:
 	std::condition_variable cv_;
 public:
 	std::map < std::string, std::list < message_t > > messages_; // multiple message queues, for every registered listener
-	void send_message( const message_t &m ) {
+	void send_message( const message_t &m, const std::string &to_id = "" ) {
 		std::lock_guard<std::mutex> lock( messages_m_ );
-		for ( auto &l : messages_ ) {
-			if ( l.second.size() > 100 ) l.second.pop_front();
-			l.second.push_back( m );
+		if ( to_id == "" ) { // if we broadcast
+			for ( auto &l : messages_ ) {
+				if ( l.second.size() > 100 ) l.second.pop_front();
+				l.second.push_back( m );
+			}
+		} else { // we send to one recipient
+			if ( messages_[to_id].size() > 100 ) messages_[to_id].pop_front();
+			messages_[to_id].push_back( m );
 		}
 		cv_.notify_all();
 	}
 
 	std::list<message_t> wait_message( const std::string &id_, int timeout = 10000 ) {
-		{
-			std::lock_guard<std::mutex> lock( messages_m_ );
-			if ( messages_[id_].size() > 0 ) {
-				auto ret = messages_[id_];
-				messages_[id_].clear();
-				return ret;
-			}
-		}
 		std::unique_lock<std::mutex> lk( cv_m_ );
-		cv_.wait_for( lk, std::chrono::milliseconds( timeout ) );
+		cv_.wait_for( lk, std::chrono::milliseconds( timeout ), [this, id_] {return messages_[id_].size() > 0;} );
+
 		{
 			std::lock_guard<std::mutex> lock( messages_m_ );
 			auto ret = messages_[id_];
